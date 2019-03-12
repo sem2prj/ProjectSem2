b608cd403005e1d7955abed1a3a4f17c08f74bd2
duydangnguyendkt <duydangnguyen.dkt@gmail.com>
2019-03-12 13:30:46 +0700

    them file server name.txt

---
+++ PharmacyManagement/src/controller/OrderProductController.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.jfoenix.controls.JFXDatePicker;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author admin
+ */
+public class OrderProductController implements Initializable {
+
+    @FXML
+    private JFXDatePicker order_dateInvoice;
+    @FXML
+    private TextField tf_barcode;
+    @FXML
+    private TextField tf_productname;
+    @FXML
+    private TextField tf_qty;
+    @FXML
+    private TextField tf_price;
+    @FXML
+    private Button btn_add;
+    @FXML
+    private TableView<?> table_invoice;
+    @FXML
+    private Label lb_total;
+    @FXML
+    private Button btn_remove;
+    
+    
+    
+    
+    
+    private Connection con;
+    private PreparedStatement pst;
+    private ResultSet rs;
+    @FXML
+    private TextField tf_invoiceID;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        order_dateInvoice.setValue(LocalDate.now());
+    }    
+    
+    
+    
+    
+    
+}
