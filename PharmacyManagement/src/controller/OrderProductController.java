3a051dd050727638bcd34c62877e4aa26b98015a
duydangnguyendkt <duydangnguyen.dkt@gmail.com>
2019-03-29 20:48:11 +0700

    them

--- PharmacyManagement/src/controller/OrderProductController.java
+++ PharmacyManagement/src/controller/OrderProductController.java
@@ -310,26 +310,29 @@ public class OrderProductController implements Initializable {
 
     private String autoOrderID(){
         String orderID = "Order00000";
-        
+
         try {
             pst = con.prepareStatement("select max(OrderID) from Orders");
             rs = pst.executeQuery();
-            
-            if(rs.next()){
+
+            if (rs.next()) {
                 orderID = rs.getString(1);
-                int n = Integer.parseInt(orderID.substring(5)) +1 ;
-                int x = String.valueOf(n).length();
-                orderID = orderID.substring(0, 10 -x) + String.valueOf(n);
-                
-            
-            } 
+                if (orderID.isEmpty()) {
+                    orderID = "Order00000";
+                } else {
+                    int n = Integer.parseInt(orderID.substring(5)) + 1;
+                    int x = String.valueOf(n).length();
+                    orderID = orderID.substring(0, 10 - x) + String.valueOf(n);
+                }
+
+            }
             rs.close();
             pst.close();
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(OrderProductController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return orderID;
     }
     
