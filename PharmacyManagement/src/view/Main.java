823a7906e45144a8055bf1bf73451c6451251dc5
duydangnguyendkt <duydangnguyen.dkt@gmail.com>
2019-03-30 17:19:09 +0700

    fwaf

--- PharmacyManagement/src/view/Main.java
+++ PharmacyManagement/src/view/Main.java
@@ -38,84 +38,49 @@ public class Main extends Application {
 ////            stage.setResizable(false);
 //            stage.show();
             
-            con = controller.ConnectDB.connectSQLServer();
-            String sql = "INSERT INTO stockdetail2(stockid,Pid,Qty,ExOutDate)";
-            pst 
 
-<<<<<<< HEAD
-=======
+
+
+
+        //Start
+
+       
+
+        pst = con.prepareStatement("select * from Users");
+        rs = pst.executeQuery();
+        
         if (rs.next()) {
             rs.close();
             pst.close();
             con.close();
             Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
 
-            Image applicationIcon = new  Image(getClass().getResource("/image/Logohygiea.png").toURI().toURL().toString(), 40, 200, true, true);
-            
+            Image applicationIcon = new Image(getClass().getResourceAsStream("/image/Login-icon.png"));
             stage.getIcons().add(applicationIcon);
-//            stage.setResizable(false);
+            stage.setResizable(false);
             Scene scene = new Scene(root);
             scene.getStylesheets().add(getClass().getResource("/css/login.css").toExternalForm());
             stage.setTitle("Login");
-//            stage.initStyle(StageStyle.DECORATED);
+            stage.initStyle(StageStyle.DECORATED);
             stage.setScene(scene);
             stage.show();
->>>>>>> 45524ea959edf5bdef5476402008d1d0fdd4896d
 
+        } else {
 
-        
+            rs.close();
+            pst.close();
+            con.close();
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Register.fxml"));
 
+            Scene scene = new Scene(root);
+//            stage.initStyle(StageStyle.UNDECORATED);
 
-<<<<<<< HEAD
-=======
-            scene.getStylesheets().add(getClass().getResource("/css/register.css").toExternalForm());
+             scene.getStylesheets().add(getClass().getResource("/css/register.css").toExternalForm());
             stage.setScene(scene);
-            Image applicationIcon = new  Image(getClass().getResource("/image/Logohygiea.png").toURI().toURL().toString(), 100, 100, true, true);
-            stage.getIcons().add(applicationIcon);
-            stage.setTitle("Register");
             stage.setResizable(false);
             stage.show();
->>>>>>> 45524ea959edf5bdef5476402008d1d0fdd4896d
 
-        //Start
-
-       
-//
-//        pst = con.prepareStatement("select * from Users");
-//        rs = pst.executeQuery();
-//        
-//        if (rs.next()) {
-//            rs.close();
-//            pst.close();
-//            con.close();
-//            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
-//
-//            Image applicationIcon = new Image(getClass().getResourceAsStream("/image/Login-icon.png"));
-//            stage.getIcons().add(applicationIcon);
-//            stage.setResizable(false);
-//            Scene scene = new Scene(root);
-//            scene.getStylesheets().add(getClass().getResource("/css/login.css").toExternalForm());
-//            stage.setTitle("Login");
-//            stage.initStyle(StageStyle.DECORATED);
-//            stage.setScene(scene);
-//            stage.show();
-//
-//        } else {
-//
-//            rs.close();
-//            pst.close();
-//            con.close();
-//            Parent root = FXMLLoader.load(getClass().getResource("/fxml/Register.fxml"));
-//
-//            Scene scene = new Scene(root);
-////            stage.initStyle(StageStyle.UNDECORATED);
-//
-//             scene.getStylesheets().add(getClass().getResource("/css/register.css").toExternalForm());
-//            stage.setScene(scene);
-//            stage.setResizable(false);
-//            stage.show();
-//
-//        }
+        }
 //
 //        //End
 ////
